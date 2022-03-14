fdb9069ffc7421a478af61c46f803a03dff2298f
DONELGOODNESS <57110844+DONELGOODNESS@users.noreply.github.com>
2022-03-14 11:08:58 +0300

    Add files via upload
    modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
 
-
+$('.btn-modal').on('click', function() {
+	$('.modal').hide();
+	let $modal = $($(this).data('modal'));
+	$modal.fadeIn(400);
+	return false;
+})
+$('.modal__overlay').on('click', function() {
+	$('.modal').fadeOut(400);
+	return false;
+})
 
 
 });
